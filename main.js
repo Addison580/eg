df503ef667cff880b1f20f8d43b9088585f09f3c
Addison580 <82746288+Addison580@users.noreply.github.com>
2022-06-11 20:49:00 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function preload(){
+
+}
+function setup(){
+    canvas = createCanvas(500, 400);
+    canvas.center();
+}
+
+function draw(){
+
+}
+
+function take_Snapshot(){
+    save('image.jpg');
+}
